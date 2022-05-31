d100e09fd15865cad3b3b56fdcd80d9d2517c833
Pixelfiles <105760940+pixelfiles@users.noreply.github.com>
2022-05-31 16:12:29 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+/*==================== SHOW NAVBAR ====================*/
+const showMenu = (headerToggle, navbarId) =>{
+    const toggleBtn = document.getElementById(headerToggle),
+    nav = document.getElementById(navbarId)
+    
+    // Validate that variables exist
+    if(headerToggle && navbarId){
+        toggleBtn.addEventListener('click', ()=>{
+            // We add the show-menu class to the div tag with the nav__menu class
+            nav.classList.toggle('show-menu')
+            // change icon
+            toggleBtn.classList.toggle('bx-x')
+        })
+    }
+}
+showMenu('header-toggle','navbar')
+
+/*==================== LINK ACTIVE ====================*/
+const linkColor = document.querySelectorAll('.nav__link')
+
+function colorLink(){
+    linkColor.forEach(l => l.classList.remove('active'))
+    this.classList.add('active')
+}
+
+linkColor.forEach(l => l.addEventListener('click', colorLink))
+
+notavailable = document.getElementById('not_available')
+not_available.addEventListener("click",demo)
+function demo(){
+    alert("There was an eror loading Android development blogs. Try again later");
+}
